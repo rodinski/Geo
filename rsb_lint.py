243fe8b15e6a73e14f7845158c546b4577089290
rodinski <crimson.blue.2@gmail.com>
2023-09-23 08:36:04 -0500

    removed remoted files that need not be there

--- rsb_lint.py
+++
@@ -1,2 +0,0 @@
-************* Module random_survey_book
-random_survey_book.py:66:14: E0001: invalid syntax (<unknown>, line 66) (syntax-error)
--- test_07_conjugate_angle.py
+++
@@ -1,6 +0,0 @@
-import Geo
-
-
-for i in range(-360, 360, 10):
-    a = Geo.Angle(i, unit='deg')
-    print(f"{a=}    {Geo.conjugate_angle(a)=}")
