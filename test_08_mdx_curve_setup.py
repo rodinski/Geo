5da2a65ba99d60f3bd0e943a2cef2d0d071f04a8
rodinski <crimson.blue.2@gmail.com>
2023-09-25 20:40:48 -0500

    more logic to loops

--- test_08_mdx_curve_setup.py
+++ test_08_mdx_curve_setup.py
@@ -12,118 +12,102 @@ from curve_intersection import ray_curve_intersect
 
 
 # setup for defaultdict to always produce a dict
+class my_defaultdict(defaultdict):
+    def __int__(self, mycallable):
+        self.defaultdict(mycallable)
 
-nested_dict = lambda: defaultdict(nested_dict)
+    def __str__(self):
+        ret_str = f"{self.items()=}"
+        return ret_str
+
+nested_dict = lambda: my_defaultdict(nested_dict)
 t = nested_dict()
 
+        
 def walk_tree(inTree):
     for k, v in inTree.items():
-        if isinstance(v, dict):
-            print(f"got on {v=}")
+        print(k, v, type(v) )
+        if isinstance(v, my_defaultdict ):
+            print(f"\n{k=}\t{v=}")
             walk_tree(v)
 
 
 R = 900
-GDSPC = 8.0
+girder_spacing = 8.0
 n_girders = 4
 delta_curve = Geo.Angle(-45, unit='deg')
 spans = itertools.accumulate([200, 200, 150], initial=10)
-sweeps = [90, 60, 80, 30]
+sweeps = [90, 60, 60, 30]
 
 cc = Point(1000, 0)
-t['cc'] = cc
 pc = Point.from_complex(cc + complex(-R, 0))
+t['cc'] = cc
+t['pc'] = pc
 curve_1 = Curve(pc, cc, delta_curve)
+t['curve_1'] = curve_1
 
-pts = [pc, curve_1.PT, cc]
-brg = 0
+pts = [cc, pc, curve_1.PT]
+
+bent = 0   # will increment
 for span, sweep in zip(spans, sweeps):
-    brg += 1
+    bent += 1
     print(f"{span=}    {sweep=}")
     print(span)
-    pt = curve_1.move_to(span, 0)
-    ref = t['pts']['bent'][brg]
-    ref['pt'][1] = pt
-    brg_brg = curve_1.tangent_at_point(pt) + \
-              Angle(sweep, unit='deg')
+    pt = curve_1.move_to(span, 0)  # locate bents on curve_1 
+    ref = t['pts']['bent'][bent] 
+    ref['pt']['mdx_1'] = pt              # far point on Ray
+    brg_brg = curve_1.tangent_at_point(pt) + Angle(sweep, unit='deg')
     ref['ray']  = Ray(pt, brg_brg)
-    ref['pt'][2] = ref['ray'].move_to(40)
+    ref['pt']['left'] = ref['ray'].move_to(100) 
+    ref['pt']['right'] = ref['ray'].move_to(-100) 
     pts.append(pt)
-    for i in [1, 2]:
-        pts.append(ref['pt'][i])
+    for item in ['mdx_1', 'left', 'right']:            
+        pts.append(ref['pt'][item])
+        
+    girder_spacing = 8.0
+    for i in range(1, 5):  # loop over girders 1,2,3,4
+        mdx_g = i + 1
+        delta_R = i*girder_spacing
+        R_i = R + delta_R
+
+        c_ref = t['mdx_g'][mdx_g]['circle'] = Curve(cc+R_i, cc, -2*pi)
+        r_ref = t['pts']['bent'][bent]['ray']
+        pts.append( ray_curve_intersect(r_ref, c_ref)[1])    # second of two intersections
+
+
+
+
+
+
 brace=itertools.accumulate([20, 20, 20, 20 ,20,
-                           20, 20, 20, 20 , 40,
-                           20, 20, 20, 20 , 20,
-                           20, 20, 20   ], initial=10)
+                            20, 20, 20, 40, 20,
+                            20, 20, 20, 20, 20,
+                            20, 20              ], initial=10 + 20)
+
+t['int_braces']['mdx'][1] = []
+ref = t['int_braces']['mdx'][1]    # easier typing
+brace_pts = []
 for b in brace:
     print(b)
-    pts.append(curve_1.move_to(b, 0))
+    b_pt = curve_1.move_to(b, 0)
+    brace_pts.append(b_pt)
+    ref.append(b_pt)
 
+for mdx_g in [2, 3, 4, 5]:
+    c_ref = t['mdx_g'][mdx_g]['circle']
+    for pt in t['int_braces']['mdx'][1]:   #each brace on mdx_1
+        r_ref = Ray(cc, Segment(cc, pt).Bearing())
+        pt = ray_curve_intersect(r_ref, c_ref)[1]
+        brace_pts.append(pt)
 
-GDSPC = 8.0
-for i in range(1, 5):
-    mdx_g = i + 1
-    delta_R = i*GDSPC
-    R_i = R + delta_R
-
-    t[mdx_g][i]['circle'] = Curve(cc+R_i, cc, -2*pi)
-    c_ref = t[mdx_g][i]['circle']
-    r_ref = t['pts']['bent'][2]['ray']
-    print(ray_curve_intersect(r_ref, c_ref) )
-    pts.append( ray_curve_intersect(r_ref, c_ref)[1])
-
+    
 
 #print(f"\n\n{dict(t)=}")
 
 fig, ax = plt.subplots()
 ax.scatter(*xy(pts))
+ax.scatter(*xy(brace_pts), marker='+', color='r')
 plt.axis('scaled')
 plt.show()
 
-
-
-
-"""
-fig, ax = plt.subplots()
-for i,R in enumerate([900, 908, 916, 924]): #radui
-    mdx = "mdx_" + str(i + 1)
-    pc = Point(1000-R,0)
-    curve=Curve(pc, cc, delta_curve)
-    d[mdx]['curves'].append(curve)
-    d[mdx]['ch'] = Chain( mdx, curve )
-    ax.add_patch( curve.patch() )
-
-
-#for R in d.keys():
-#    print( R )
-#    myChain = d[R]['ch']
-#    #ax.scatter( [myCurve.PC.X], [myCurve.PC.Y], marker='o', color='g')
-#    #ax.scatter( [myCurve.PT.X], [myCurve.PT.Y], marker='o', color='r')
-#    #ax.scatter( Geo.as_XY(points)[0], Geo.as_XY(points)[1], marker='+', color='g' )
-#    #ax.scatter( Geo.as_XY(bad_pts)[0], Geo.as_XY(bad_pts)[1], marker='.', color='r' )
-#    for rt in myChain.Routes:
-#        ax.add_patch( rt.patch() )
-
-bts = itertools.accumulate( [ 200,200,], initial=10)
-sweeps = [90, 60, 80]
-for bt,sweep in zip(bts, sweeps):
-    ref = d['mdx_1']
-    ref['pts'].append( ref['ch'].move_to(bt,0) )
-    tangent = Segment(cc, ref['pts'][-1] ).Bearing().rt_90()
-    brg_line = tangent + Angle(sweep, unit='deg')
-    ref['pts'].append(Point.from_complex( ref['pts'][-1] + cmath.rect(100, brg_line)))
-    print(ref['pts'])
-    
-    ref['seg'].append( Segment (ref['pts'][-2], ref['pts'][-1] ))
-    print(ref['seg'])
-    ax.add_patch( ref['seg'][-1].patch() )
-
-for key in d.keys():
-    pt = d[key]['pts']
-    ax.scatter( Geo.as_XY(pt)[0], Geo.as_XY(pt)[1], marker='o', color='b' )
-
-print(bts)
-plt.axis('scaled')
-plt.show()
-"""
 # IPython.embed()
