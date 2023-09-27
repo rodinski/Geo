5b186535e4737f1f80e68a09aacf752330f7a9f6
rodinski <crimson.blue.2@gmail.com>
2023-09-26 21:40:17 -0500

    take out space in mini-language   :, f}    to :,f}

--- Geo.py
+++ Geo.py
@@ -169,7 +169,7 @@ class Angle(float):
         return '%g rad  %g deg' % (self, math.degrees(self))
 
     def __repr(self):
-        return "Angle({self:, f})"
+        return "Angle({self:,f})"
 
 
 class Distance(float):
@@ -180,7 +180,7 @@ class Distance(float):
         self.val = round(flt, Decimals)
 
     def __repr__(self):
-        return f"Distance({self.val:, f})"
+        return f"Distance({self.val:,f})"
 
 
 class Bearing(float):
