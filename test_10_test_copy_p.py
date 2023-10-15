7106028234cc7d79e31e6d70f3caada722ad4f5b
rodinski <crimson.blue.2@gmail.com>
2023-10-15 08:28:05 -0500

    test_10  added

--- test_10_test_copy_p.py
+++ test_10_test_copy_p.py
@@ -15,10 +15,10 @@ seg.append(seg[0].copy_parallel(+110))
 #print(seg[-1])
 print(len(seg))
 
-cur.append(Geo.Curve(pts[0], pts[1], Geo.Angle(5, unit='deg')))
+cur.append(Geo.Curve(pts[0], pts[1], Geo.Angle(-70, unit='deg')))
 cur.append(cur[0].copy_parallel(-30))
 cur.append(cur[0].copy_parallel(-60))
-cur.append(cur[0].copy_parallel(-180))
+cur.append(cur[0].copy_parallel(10))
 
 ray.append(Geo.Ray(Geo.Point(12,3), 0.14))
 ray.append(ray[0].copy_parallel(-10))
